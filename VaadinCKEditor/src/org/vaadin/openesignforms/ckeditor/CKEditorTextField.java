37c51543ba4423a47b3a2ad04f89b95733052c35
Matti Tahvonen <matti@vaadin.com>
2016-03-25 16:21:56 +0200

    Send text to client, only when it has actaully changed from the server side

--- VaadinCKEditor/src/org/vaadin/openesignforms/ckeditor/CKEditorTextField.java
+++ VaadinCKEditor/src/org/vaadin/openesignforms/ckeditor/CKEditorTextField.java
@@ -47,6 +47,8 @@ public class CKEditorTextField extends AbstractField<String>
 	private boolean focusRequested = false;
 	protected LinkedList<VaadinSaveListener> vaadinSaveListenerList;
 
+	private boolean textIsDirty;
+
 	public CKEditorTextField() {
 		super.setValue("");
 		setWidth("100%");
@@ -80,15 +82,27 @@ public class CKEditorTextField extends AbstractField<String>
     		newValue = "";
     	super.setValue(newValue, false);
     	requestRepaint();
+    	textIsDirty = true;
     }
+	
+	@Override
+	public void beforeClientResponse(boolean initial) {
+		if(initial) {
+			textIsDirty = true;
+		}
+		super.beforeClientResponse(initial);
+	}
 	
 	@Override
 	public void paintContent(PaintTarget target) throws PaintException {
 		//super.paintContent(target);
 		
-		Object currValueObject = getValue();
-		String currValue = currValueObject == null ? "" : currValueObject.toString();
-		target.addVariable(this, VCKEditorTextField.VAR_TEXT, currValue);
+		if(textIsDirty) {
+			Object currValueObject = getValue();
+			String currValue = currValueObject == null ? "" : currValueObject.toString();
+			target.addVariable(this, VCKEditorTextField.VAR_TEXT, currValue);
+			textIsDirty = false;
+		}
 		
 		target.addAttribute(VCKEditorTextField.ATTR_READONLY, isReadOnly());
 		target.addAttribute(VCKEditorTextField.ATTR_VIEW_WITHOUT_EDITOR, isViewWithoutEditor());
